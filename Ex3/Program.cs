8098646d33e9624079bff5b6d97de6ff36d34de8
Inna Glebova <into4kand@mail.ru>
2023-04-30 21:38:02 +0300

    Third Ex done

---
+++ Ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 0)
+{
+    Console.WriteLine("Квадраты всех чисел от 1 до N");
+}
+else
+{
+    Console.WriteLine("Введите число больше 0");
+}
+for (int i = 1; i <= N; i++)
+{
+    int Q = i*i;
+    Console.Write($"{Q} ");
+}
