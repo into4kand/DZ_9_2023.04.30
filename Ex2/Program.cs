330fbd8d5b905c5b95d509336aaa959890b8c166
Inna Glebova <into4kand@mail.ru>
2023-04-30 21:22:08 +0300

    Second Ex done

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+int[] FirstDot = new int [3];
+Console.WriteLine("Введите коррдинаты X, Y, Z первой точки ");
+for (int i = 0; i < FirstDot.Length; i++)
+{
+    FirstDot[i] = Convert.ToInt32(Console.ReadLine());
+}
+int[] SecondDot = new int [3];
+Console.WriteLine("Введите коррдинаты  X, Y, Z второй точки ");
+for (int i = 0; i < SecondDot.Length; i++)
+{
+    SecondDot[i] = Convert.ToInt32(Console.ReadLine());
+}
+int a = 0;
+for (int i = 0; i < SecondDot.Length; i++)
+{
+    a = a + (SecondDot[i] - FirstDot[i])*(SecondDot[i] - FirstDot[i]);
+}
+double S = Math.Sqrt(a);
+Console.WriteLine($"Расстояние между двумя точками равно {S}");
